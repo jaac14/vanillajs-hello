160c42f469460743206fc54846fc9804b62bae16
jaac14 <124366885+jaac14@users.noreply.github.com>
2023-08-12 12:14:35 +0000

    first Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  window.onload = function() {
+    //write your code here
+    let who = ["The dog", "My grandma", "His turtle", "My bird", "gitpod"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let excuses = `${randomArrays(who)} ${randomArrays(action)} ${randomArrays(
+      what
+    )} ${randomArrays(when)}`;
+    let element = document.getElementById("excuse");
+    element.innerHTML = excuses;
+  };
+  function randomArrays(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
 };
